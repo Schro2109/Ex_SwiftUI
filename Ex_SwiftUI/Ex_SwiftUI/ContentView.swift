b1eb0024a24c2b25ffb508408cafc1a5742b108a
Schro2109 <bloodparty297@gmail.com>
2023-06-02 14:28:24 +0900

    add: 프로젝트 생성 및 STEP1 완료

---
+++ Ex_SwiftUI/Ex_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ex_SwiftUI
+//
+//  Created by Schro on 2023/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ex_SwiftUI/Ex_SwiftUI/Ex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex_SwiftUIApp.swift
+//  Ex_SwiftUI
+//
+//  Created by Schro on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct Ex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
